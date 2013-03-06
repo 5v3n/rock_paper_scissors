7137021d4475b2f5eb0a0029413c4569c16b187d
Sven Kraeuter | 5v3n <sven.kraeuter@gmail.com>
2013-03-06 22:44:38 +0100

    specs for Strategy::Base

--- lib/strategy/base.rb
+++ lib/strategy/base.rb
@@ -3,7 +3,7 @@ require_relative '../rock_paper_scissors.rb'
 module RockPaperScissors::Strategy 
   class Base
     def choice
-      raise "not yet implemented"
+      raise "not yet specified"
     end
   end
 end
--- spec/strategy/base_spec.rb
+++ spec/strategy/base_spec.rb
@@ -3,4 +3,9 @@ require 'spec_helper'
 require_relative '../../lib/strategy/base.rb'
 
 describe RockPaperScissors::Strategy::Base do
+  context ".choice()" do
+    it "raises an error pointing out it has to be specified" do
+      lambda {RockPaperScissors::Strategy::Base.new.choice()}.should raise_error(RuntimeError, /not yet specified/)
+    end
+  end
 end
