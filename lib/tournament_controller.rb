9bf7abdf9058a77121c5b5902f4f0650419b6d2b
Sven Kraeuter | 5v3n <sven.kraeuter@gmail.com>
2013-03-07 00:52:16 +0100

    introducing controllet (i really tried not to)

--- lib/tournament_controller.rb
+++ lib/tournament_controller.rb
@@ -1,12 +1,21 @@
 require 'erb'
 require_relative 'tournament'
 
-results       = RockPaperScissors::Tournament.play
-player_a_wins = results[:wins_player_a]
-player_b_wins = results[:wins_player_b]
-total_rounds  = results[:rounds]
-ties          = results[:ties]
+class TournamentController
+  def index
+    results       = RockPaperScissors::Tournament.play
+    @player_a_wins = results[:wins_player_a]
+    @player_b_wins = results[:wins_player_b]
+    @total_rounds  = results[:rounds]
+    @ties          = results[:ties]
+  end
+  def get_binding # this is only a helper method to access the objects binding method
+    binding
+  end
+end
 
-template = ERB.new File.new(File.expand_path("../tournament_view.erb", __FILE__)).read
-
-puts template.result(binding)
+controller = TournamentController.new
+controller.index
+template_string = File.new(File.expand_path("../tournament_view.erb", __FILE__)).read
+template = ERB.new template_string
+puts template.result(controller.get_binding)
