77bfd5431b8543aee4fbbca40aa2e34e090823f4
Sven Kraeuter | 5v3n <sven.kraeuter@gmail.com>
2013-03-07 00:45:05 +0100

    moving template to own file

--- lib/tournament_controller.rb
+++ lib/tournament_controller.rb
@@ -7,10 +7,6 @@ player_b_wins = results[:wins_player_b]
 total_rounds  = results[:rounds]
 ties          = results[:ties]
 
-template = ERB.new <<-EOF
-  Player A wins <%= player_a_wins %> of <%= total_rounds %> games
-  Player B wins <%= player_b_wins %> of <%= total_rounds %> games
-  Tie: <%= ties %> of <%= total_rounds %> games"
-EOF
+template = ERB.new File.new(File.expand_path("../tournament_view.erb", __FILE__)).read
 
 puts template.result(binding)
